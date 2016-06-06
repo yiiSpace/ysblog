82caa603dc7d2a442634edef6bc595b630031c1e
yiqing <yiqing_95@qq.com>
2016-06-06 22:48:58 +0800

    发布时间 格式化为中文

--- my/blog/frontend/widgets/views/_item.php
+++ my/blog/frontend/widgets/views/_item.php
@@ -12,20 +12,23 @@ use yii\widgets\DetailView;
 ?>
 <div class="divider"></div>
 <div class="section">
-    <h5><?= $model->title ?></h5>
+    <h5><?= Html::a( Html::encode($model->title),['/blog/entry/view', 'id' => $model->primaryKey],['target'=>'_blank'] ) ?></h5>
 
     <p><?= $model->getTease() ?></p>
 
     <div class="article_manage right-align">
-        <span class="link_postdate">2016-05-29 21:14</span>
+        <span class="link_postdate"><?= Yii::$app->formatter->asDatetime( $model->created_at ) ;?></span>
 
 
-                                <span title="阅读次数" class="link_view"><a title="阅读次数"
-                                                                        href="/yanghua_kobe/article/details/51533957">阅读</a>(8853)</span>
-                                <span title="评论次数" class="link_comments"><a
-                                        onclick="_gaq.push(['_trackEvent','function', 'onclick', 'blog_articles_pinglun'])"
-                                        title="评论次数"
-                                        href="/yanghua_kobe/article/details/51533957#comments">评论</a>(0)</span>
+                                <span title="阅读次数" class="link_view">
+                                    <a title="阅读次数"
+                                       href="<?= \yii\helpers\Url::to(['/blog/entry/view', 'id' => $model->primaryKey]) ?>">阅读</a>(8853)</span>
+                                <span title="评论次数" class="link_comments">
+                                    <a onclick=""
+                                       title="评论次数"
+                                       href="/yanghua_kobe/article/details/51533957#comments">
+                                        评论</a>(0
+                                    )</span>
 
     </div>
 </div>
